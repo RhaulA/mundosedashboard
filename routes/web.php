65515fb23dee433a5549db9d0de68ff25cb0ef5c
RhaulA <80433033+RhaulA@users.noreply.github.com>
2021-11-19 16:04:58 -0300

    creación de controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.home');
+Route::get('/', "DashboardController@index")->name("dashboard");
+/*
+Route::prefix('dashboard')->group(funcion () {
+    Route::get('/', "DashboardController@index");
 });
+*/
+Route::resourse('About',"AboutController");
