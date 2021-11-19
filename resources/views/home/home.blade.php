529299c918394ad4f6537409873a333155b1d48e
RhaulA <80433033+RhaulA@users.noreply.github.com>
2021-11-19 10:51:05 -0300

    primer commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,8 @@
+@extends('base')
+@section('title') Inicio @endsection
+@section('content')
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class="display-4">Dashboard</h1>
+        </div>
+    </main>
+@endsection
